cf589771ea6316f9126b4b8d713aba5a55bfda28
Vasizet <cirotendelei@gmail.com>
2025-01-14 14:23:05 +0100

    First commit

---
+++ hypr/scripts/hyprshot.sh
@@ -0,0 +1,1 @@
+hyprshot -m output -o /home/nic/screenshot
---
+++ hypr/scripts/waybar.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+killall waybar
+
+waybar --config /home/nic/.config/hypr/waybar/config.jsonc --style /home/nic/.config/hypr/waybar/style.css 
